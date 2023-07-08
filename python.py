b7bde459744e75e98318381eb9e385a60c8c6da7
kingyorksh <galcivar4572@utm.edu.ec>
2023-07-08 00:45:55 -0500

    Se creo la clase Metodos() en el archivo Listas

--- python.py
+++ python.py
@@ -1,10 +1,6 @@
-git config --list  
-
-git config --global user.name "kingyorksh"
-git config --global user.email "galcivar4572@utm.edu.ec"
-
-git init
-
-git config --global -e       # vemos las configuracions 
-
-git push --set-upstream origin main
+from Listas import Metodos
+Listas=[]
+_tam=int(input("Ingrese el tamano de la Lista: "))
+c=Metodos()
+c.ingreso(Listas,_tam)
+c.impresion(Listas,_tam)
