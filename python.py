693b83b332631290610dbb2dfd372efa461fde5b
kingyorksh <galcivar4572@utm.edu.ec>
2023-07-08 00:07:16 -0500

    Creacion del File python.py de las listas a trabajar

---
+++ python.py
@@ -0,0 +1,10 @@
+git config --list  
+
+git config --global user.name "kingyorksh"
+git config --global user.email "galcivar4572@utm.edu.ec"
+
+git init
+
+git config --global -e       # vemos las configuracions 
+
+git push --set-upstream origin main
